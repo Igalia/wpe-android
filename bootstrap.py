45934f15b31c8f095880e014ed9f2086665b18bb
Fernando Jimenez <fjimenez@igalia.com>
2021-02-11 18:44:46 +0100

    Make bootstrap install all the dependencies
    Remove old binaries and headers. The bootstrap script generates
    and place all dependency files in its expected location, so there
    is no need to keep them in git.

--- bootstrap.py
+++ bootstrap.py
@@ -6,11 +6,28 @@ import shutil
 import subprocess
 import sys
 
+from pathlib import Path
+
 class Bootstrap:
     def __init__(self, arch):
         self.__arch = arch
         self.__root = os.getcwd()
         self.__build_dir = os.path.join(os.getcwd(), 'build')
+        self.__build_libs = [
+            'glib-2.0',
+            'libglib-2.0.so',
+            'libwpe-1.0.so',
+            'libWPEBackend-android.so',
+            'libWPEWebKit-1.0.so',
+            'libWPEWebKit-1.0_3.so',
+            'libWPEWebKit-1.0_3.11.7.so'
+        ]
+        self.__build_includes = [
+            'glib-2.0',
+            'wpe-1.0',
+            'wpe-android',
+            'wpe-webkit-1.0'
+        ]
 
     def __cerbero_command(self, args):
         os.chdir(self.__build_dir)
@@ -35,6 +52,9 @@ class Bootstrap:
 
         self.__cerbero_command(['bootstrap'])
 
+    def __build_deps(self):
+        self.__cerbero_command(['package', '-f', 'wpewebkit'])
+
     def __extract_deps(self):
         os.chdir(self.__build_dir)
         sysroot = os.path.join(self.__build_dir, 'sysroot')
@@ -45,17 +65,57 @@ class Bootstrap:
             file_path = os.path.join(self.__build_dir, f)
             subprocess.call(['tar', 'xvf', file_path, '-C', sysroot])
             os.remove(file_path)
-        # TODO: move deps to corresponding location. i.e libs to jniLibs folder.
 
-    def __build_deps(self):
-        self.__cerbero_command(['package', '-f', 'wpewebkit'])
+    def __install_deps(self):
+        sysroot = os.path.join(self.__build_dir, 'sysroot')
+        wpe = os.path.join(self.__root, 'wpe')
+
+        include_dir = os.path.join(wpe, 'imported', 'include')
+        if os.path.exists(include_dir):
+            shutil.rmtree(include_dir)
+        os.makedirs(include_dir)
+        for header in self.__build_includes:
+            shutil.copytree(os.path.join(sysroot, 'include', header),
+                            os.path.join(include_dir, header))
+
+        if self.__arch == 'arm64':
+            android_abi = 'arm64-v8a'
+        elif self.__arch == 'arm':
+            android_abi = 'armeabi-v7a'
+        else:
+            raise Exception('Architecture not supported')
+
+        sysroot_lib = os.path.join(sysroot, 'lib')
+        lib_dir = os.path.join(wpe, 'imported', 'lib', android_abi)
+        if os.path.exists(lib_dir):
+            shutil.rmtree(lib_dir)
+        os.makedirs(lib_dir)
+        for lib in self.__build_libs:
+            origin = os.path.join(sysroot_lib, lib)
+            dst = os.path.join(lib_dir, lib)
+            if os.path.isdir(origin):
+                shutil.copytree(origin, dst)
+            else:
+                shutil.copy(origin, dst, follow_symlinks=False)
+
+        os.symlink(os.path.join(lib_dir, 'libWPEBackend-android.so'),
+                os.path.join(lib_dir, 'libWPEBackend-default.so'))
+
+        jnilib_dir = os.path.join(wpe, 'src', 'main', 'jniLibs', android_abi)
+        if os.path.exists(jnilib_dir):
+            shutil.rmtree(jnilib_dir)
+        os.makedirs(jnilib_dir)
+        for lib_path in Path(sysroot_lib).glob('*.so'):
+            lib_name = os.path.basename(lib_path)
+            if lib_name in self.__build_libs:
+                continue
+            shutil.copy(lib_path, os.path.join(jnilib_dir, lib_name))
 
     def run(self):
         self.__ensure_cerbero()
-        for f in glob.glob('wpewebkit-android-*.tar.xz'):
-            os.remove(f)
         self.__build_deps()
         self.__extract_deps()
+        self.__install_deps()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
