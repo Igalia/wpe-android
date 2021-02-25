4b5176f782bfd2079629fa15c6aedbde8985377e
Fernando Jimenez <fjimenez@igalia.com>
2021-02-25 10:40:49 +0100

    Document the bootstrap script

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,59 @@
 #!/bin/python
 
+"""
+This script takes care of fetching, building and installing all WPE Android dependencies,
+including libwpe, WPEBackend-android and WPEWebKit.
+
+The cross-compilation work is done by Cerbero: https://gitlab.igalia.com/ferjm/cerbero
+
+After cloning Cerbero's source through git in the `build` folder, the process starts with
+the following Cerbero command:
+
+`./cerbero-uninstalled -c config/cross-android-<android_abi> -f wpewebkit`
+
+where `<android_abi>` varies depending on the given architecture target.
+
+The logic for this command is in the WPEWebKit packaging recipe in Cerbero's repo:
+https://gitlab.igalia.com/ferjm/cerbero/-/blob/b9c3b76efb1ed7e2fedfcd6838e638a194df2da8/packages/wpewebkit.package
+
+This command triggers the build for all WPEWebKit dependencies. After that WPEWebKit itself
+is built. You can find the recipes for all dependencies and WPEWebKit build in the
+`recipes` folder of Cerbero's repo.
+
+Once WPEWebKit and all dependencies are built, the pre-package step is executed.
+
+The pre-packaging step tries to work around a limitation of Android's package manager.
+Android package manager only unpacks libxxx.so named libraries so any library with
+versioning (i.e. libxxx.so.1) will be ignored. To fix this we rename all versioned
+libraries to the libxxx.so form. For example, a library named libfoo.so.1 will become
+libfoo_1.so. Apart from renaming the actual library files, we need to tweak the
+SONAME and NEEDED values as well to reflect the name changes.
+
+After pre-package, the actual packaging starts. This takes care of packaging the result of
+cross-compiling all the dependencies. The list of assets that are packaged is defined by
+the `files` variable in the packaging recipe. The syntax `wpeandroid:libs:stl` means
+'from the recipe wpeandroid, include the libraries (`files_libs` in the recipe) and the
+STL lib (`files_stl` in the recipe). You can think of the `:` separating the file types
+as commas in a list. For most recipes we only care about the libraries, except for
+WPEWebKit from which we want everything.
+
+Two different tar files are generated during this process. One containing the runtime
+assets and another one with the development assets.
+
+After packaging, the post-package step is executed. During this step we need to undo the
+changes done in the pre-packaging step. This is restoring the versioned libraries references
+in Cerbero's sysroot. After that we need to process the generated runtime package to
+rename the versioned libraries from the libxxx.so.1 form to the libxxx_1.so one to match
+the changes done during the pre-packaging step. We also need to take care of the symbolic
+links to reflect the naming changes.
+
+After that the packaging work is complete and we are done with Cerbero.
+
+The next step is extracting the packages in the `build/sysroot` folder and copying its content
+in the appropriate folders. This is done by the `__install_deps` function.
+
+"""
+
 import glob
 import os
 import shutil
@@ -13,7 +67,10 @@ class Bootstrap:
         self.__arch = arch
         self.__root = os.getcwd()
         self.__build_dir = os.path.join(os.getcwd(), 'build')
-        self.__build_libs = [
+        # These are the libraries that the glue code link with,
+        # that go into the `imported` folder and cannot go into
+        # the `jniFolder` to avoid a duplicated library issue.
+       self.__build_libs = [
             'glib-2.0',
             'libglib-2.0.so',
             'libwpe-1.0.so',
@@ -41,6 +98,8 @@ class Bootstrap:
         subprocess.call(command)
 
     def __ensure_cerbero(self):
+        # TODO: change this to a public URL once we publish the
+        #       cerbero changes
         origin = 'ssh://git@gitlab.igalia.com:4429/ferjm/cerbero.git'
         branch = 'wpe-android'
 
