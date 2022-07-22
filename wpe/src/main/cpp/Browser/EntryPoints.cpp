19525c54a2abf846a21554349126d9d5ea48db7e
Jani HautakangaS <jani@igalia.com>
2022-07-22 18:41:53 +0300

    Map native calls directly from Page.java to page.cpp
    This patch introduces direct one to one mapping between Page java
    object and Page c++ object. There's no need to keep c++ Page
    instances in hashmap and fetch them for every call. This direct mapping
    reduces complexitity of code and state handling greatly.

--- wpe/src/main/cpp/Browser/EntryPoints.cpp
+++ wpe/src/main/cpp/Browser/EntryPoints.cpp
@@ -3,10 +3,10 @@
 #include "JNIHelper.h"
 #include "Logging.h"
 #include "LooperThread.h"
+#include "Page.h"
 #include "PageEventObserver.h"
 #include "Service.h"
 
-#include <android/native_window_jni.h>
 #include <wpe/wpe.h>
 
 extern "C" {
@@ -50,147 +50,6 @@ void shut(JNIEnv* env, jclass)
         env->DeleteWeakGlobalRef(s_browserGlue_object);
         s_browserGlue_object = nullptr;
     }
-}
-
-void newPage(JNIEnv* env, jclass, jobject pageObj, jint pageId, jint width, jint height)
-{
-    ALOGV("BrowserGlue::newPage(%p, %d, %d, %d) [tid %d]", pageObj, pageId, width, height, gettid());
-
-    jclass pageClass = env->GetObjectClass(pageObj);
-    Browser::getInstance().newPage(pageId, width, height, std::make_shared<PageEventObserver>(env, pageClass, pageObj));
-
-    jmethodID onReady = env->GetMethodID(pageClass, "onPageGlueReady", "()V");
-    if (onReady != nullptr) {
-        env->CallVoidMethod(pageObj, onReady);
-        if (env->ExceptionCheck()) {
-            env->ExceptionDescribe();
-            env->ExceptionClear();
-        }
-    }
-    env->DeleteLocalRef(pageClass);
-}
-
-void closePage(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::closePage(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().closePage(pageId);
-}
-
-void loadURL(JNIEnv* env, jclass, jint pageId, jstring url)
-{
-    const char* urlChars = env->GetStringUTFChars(url, nullptr);
-    ALOGV("BrowserGlue::loadURL(%d, %s) [tid %d]", pageId, urlChars, gettid());
-    jsize urlLength = env->GetStringUTFLength(url);
-    Browser::getInstance().loadUrl(pageId, urlChars, urlLength);
-    env->ReleaseStringUTFChars(url, urlChars);
-}
-
-void goBack(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::goBack(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().goBack(pageId);
-}
-
-void goForward(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::goForward(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().goForward(pageId);
-}
-
-void stopLoading(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::stopLoading(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().stopLoading(pageId);
-}
-
-void reload(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::reload(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().reload(pageId);
-}
-
-void surfaceCreated(JNIEnv* env, jclass, jint pageId, jobject jSurface)
-{
-    ALOGV("BrowserGlue::surfaceCreated(%d, %p) [tid %d]", pageId, jSurface, gettid());
-    Browser::getInstance().surfaceCreated(pageId, ANativeWindow_fromSurface(env, jSurface));
-}
-
-void surfaceChanged(JNIEnv*, jclass, jint pageId, jint format, jint width, jint height)
-{
-    ALOGV("BrowserGlue::surfaceChanged(%d, %d, %d, %d) [tid %d]", pageId, format, width, height, gettid());
-    Browser::getInstance().surfaceChanged(pageId, format, width, height);
-}
-
-void surfaceRedrawNeeded(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::surfaceRedrawNeeded(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().surfaceRedrawNeeded(pageId);
-}
-
-void surfaceDestroyed(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::surfaceDestroyed(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().surfaceDestroyed(pageId);
-}
-
-void touchEvent(JNIEnv*, jclass, jint pageId, jlong time, jint type, jfloat x, jfloat y)
-{
-    ALOGV(
-        "BrowserGlue::touchEvent(%d, %ld, %d, %f, %f) [tid %d]", pageId, static_cast<long>(time), type, x, y, gettid());
-    Browser::getInstance().onTouch(pageId, time, type, x, y);
-}
-
-void setZoomLevel(JNIEnv*, jclass, jint pageId, jdouble zoomLevel)
-{
-    ALOGV("BrowserGlue::setZoomLevel(%d, %f) [tid %d]", pageId, zoomLevel, gettid());
-    Browser::getInstance().setZoomLevel(pageId, zoomLevel);
-}
-
-void setInputMethodContent(JNIEnv*, jclass, jint pageId, jchar c)
-{
-    ALOGV("BrowserGlue::setInputMethodContent(%d, %c) [tid %d]", pageId, c, gettid());
-    Browser::getInstance().setInputMethodContent(pageId, c);
-}
-
-void deleteInputMethodContent(JNIEnv*, jclass, jint pageId, jint offset)
-{
-    ALOGV("BrowserGlue::deleteInputMethodContent(%d, %d) [tid %d]", pageId, offset, gettid());
-    Browser::getInstance().deleteInputMethodContent(pageId, offset);
-}
-
-void requestExitFullscreenMode(JNIEnv*, jclass, jint pageId)
-{
-    ALOGV("BrowserGlue::requestExitFullscreen(%d) [tid %d]", pageId, gettid());
-    Browser::getInstance().requestExitFullscreen(pageId);
-}
-
-void updateAllPageSettings(JNIEnv* env, jclass, jint pageId, jobject jPageSettings)
-{
-    ALOGV("BrowserGlue::updateAllPageSettings(%d) [tid %d]", pageId, gettid());
-
-    PageSettings settings;
-
-    jclass jPageSettingsClass = env->GetObjectClass(jPageSettings);
-
-    jmethodID methodID = env->GetMethodID(jPageSettingsClass, "getUserAgentString", "()Ljava/lang/String;");
-    if (methodID != nullptr) {
-        jstring jniString = reinterpret_cast<jstring>(env->CallObjectMethod(jPageSettings, methodID));
-        const char* str = env->GetStringUTFChars(jniString, nullptr);
-        settings.setUserAgent(str);
-        env->ReleaseStringUTFChars(jniString, str);
-    } else
-        ALOGE("Cannot update user agent setting (cannot find \"getUserAgentString\" method)");
-
-    methodID = env->GetMethodID(jPageSettingsClass, "getMediaPlaybackRequiresUserGesture", "()Z");
-    if (methodID != nullptr) {
-        jboolean require = env->CallBooleanMethod(jPageSettings, methodID);
-        settings.setMediaPlayerRequiresUserGesture(require);
-    } else
-        ALOGE("Cannot update media playback requires user gesture setting (cannot find \"getUserAgentString\" method)");
-
-    env->DeleteLocalRef(jPageSettingsClass);
-
-    Browser::getInstance().updateAllPageSettings(pageId, settings);
 }
 
 // Process management API
@@ -328,29 +187,18 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved)
     if (klass == nullptr)
         return JNI_ERR;
 
-    static const JNINativeMethod methods[] = {
-        {"setupEnvironment", "([Ljava/lang/String;)V", reinterpret_cast<void*>(setupEnvironment)},
-        {"init", "(Lcom/wpe/wpe/BrowserGlue;)V", reinterpret_cast<void*>(init)},
-        {"initLooperHelper", "()V", reinterpret_cast<void*>(initLooperHelper)},
-        {"shut", "()V", reinterpret_cast<void*>(shut)},
-        {"newPage", "(Lcom/wpe/wpe/Page;III)V", reinterpret_cast<void*>(newPage)},
-        {"closePage", "(I)V", reinterpret_cast<void*>(closePage)},
-        {"loadURL", "(ILjava/lang/String;)V", reinterpret_cast<void*>(loadURL)},
-        {"goBack", "(I)V", reinterpret_cast<void*>(goBack)}, {"goForward", "(I)V", reinterpret_cast<void*>(goForward)},
-        {"stopLoading", "(I)V", reinterpret_cast<void*>(stopLoading)},
-        {"reload", "(I)V", reinterpret_cast<void*>(reload)},
-        {"surfaceCreated", "(ILandroid/view/Surface;)V", reinterpret_cast<void*>(surfaceCreated)},
-        {"surfaceChanged", "(IIII)V", reinterpret_cast<void*>(surfaceChanged)},
-        {"surfaceRedrawNeeded", "(I)V", reinterpret_cast<void*>(surfaceRedrawNeeded)},
-        {"surfaceDestroyed", "(I)V", reinterpret_cast<void*>(surfaceDestroyed)},
-        {"touchEvent", "(IJIFF)V", reinterpret_cast<void*>(touchEvent)},
-        {"setZoomLevel", "(ID)V", reinterpret_cast<void*>(setZoomLevel)},
-        {"setInputMethodContent", "(IC)V", reinterpret_cast<void*>(setInputMethodContent)},
-        {"deleteInputMethodContent", "(II)V", reinterpret_cast<void*>(deleteInputMethodContent)},
-        {"requestExitFullscreenMode", "(I)V", reinterpret_cast<void*>(requestExitFullscreenMode)},
-        {"updateAllPageSettings", "(ILcom/wpe/wpe/PageSettings;)V", reinterpret_cast<void*>(updateAllPageSettings)}};
+    static const JNINativeMethod methods[]
+        = {{"setupEnvironment", "([Ljava/lang/String;)V", reinterpret_cast<void*>(setupEnvironment)},
+            {"init", "(Lcom/wpe/wpe/BrowserGlue;)V", reinterpret_cast<void*>(init)},
+            {"initLooperHelper", "()V", reinterpret_cast<void*>(initLooperHelper)},
+            {"shut", "()V", reinterpret_cast<void*>(shut)}};
+
     int result = env->RegisterNatives(klass, methods, sizeof(methods) / sizeof(JNINativeMethod));
     env->DeleteLocalRef(klass);
+
+    if (result == JNI_OK) {
+        result = Page::registerJNINativeFunctions(env);
+    }
 
     wpe_process_provider_register_interface(&processProviderInterface);
 
