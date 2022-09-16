b5b2249c1312b392f68f115f835628d13bf00f17
Loïc Le Page <llepage@igalia.com>
2022-09-16 11:48:53 +0200

    Add LGPL2.1 license header to code files

--- tools/scripts/check-format.py
+++ tools/scripts/check-format.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+##
+# Copyright (C) 2022 Igalia S.L. <info@igalia.com>
+#   Author: Loïc Le Page <llepage@igalia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+##
+
 """
 This script checks all files names and code format for Python, Java, C/C++ and Cmake files.
 If called with --hook option, it will only check differencies with previous git commit.
