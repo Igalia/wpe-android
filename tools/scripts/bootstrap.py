7514f1b6c2adb37825fa1a878dec74768312a7ac
Loïc Le Page <llepage@igalia.com>
2023-01-10 09:49:41 +0100

    Upgrade dependencies to wpe-webkit 2.38.3

--- tools/scripts/bootstrap.py
+++ tools/scripts/bootstrap.py
@@ -81,7 +81,7 @@ from urllib.request import urlretrieve
 
 class Bootstrap:
     default_arch = "arm64"
-    default_version = "2.38.2"
+    default_version = "2.38.3"
 
     _cerbero_origin = "https://github.com/Igalia/cerbero.git"
     _cerbero_branch = "wpe-android"
