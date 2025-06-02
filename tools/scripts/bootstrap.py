3d4da25f303f3c85d97c21ca607d0ddd1eeb3ee3
Adrian Perez de Castro <aperez@igalia.com>
2025-06-02 13:27:21 +0300

    [wpe] Update to WPE WebKit 2.48.3
    This is a minor bugfix release, changing the versions strings is enough.

--- tools/scripts/bootstrap.py
+++ tools/scripts/bootstrap.py
@@ -81,7 +81,7 @@ from urllib.request import urlretrieve
 
 class Bootstrap:
     default_arch = "arm64"
-    default_version = "2.48.2"
+    default_version = "2.48.3"
 
     _cerbero_origin = "https://github.com/Igalia/wpe-android-cerbero.git"
     _cerbero_branch = "main"
