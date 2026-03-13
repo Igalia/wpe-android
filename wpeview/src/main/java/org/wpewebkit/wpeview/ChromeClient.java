43ee38102806c295fc8bd3bd262720e471425223
Alejandro G. Castro <alex@igalia.com>
2026-03-13 21:55:12 +0100

    Convenience API: Add new high-level API for Android
    Adds a parallel high-level API in org.wpewebkit.wpeview without changing
    the existing WPEView-based API.
      - ChromeClient.java: high-level chrome/UI callback adapter for WebView.
      - ViewClient.java: high-level navigation/resource callback adapter for
        WebView.
      - WebContext.java: shared context object wrapping the new low-level
        display, session, settings, and cookie state.
      - WebSettings.java: high-level settings facade over WebKitSettings.
      - CookieManager.java: high-level cookie policy API.
      - WebView.java: new high-level Android widget built on the WPE
        CAPI bridge.
    
    This commit is additive only: the legacy WPEView/WPEContext API remains
    unchanged and existing consumers can continue using it.

---
+++ wpeview/src/main/java/org/wpewebkit/wpeview/ChromeClient.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2026 Igalia S.L.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package org.wpewebkit.wpeview;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+/**
+ * ChromeClient provides callbacks for UI events.
+ */
+public class ChromeClient {
+    public void onCloseWindow(@NonNull WebView window) {}
+    public void onProgressChanged(@NonNull WebView view, int newProgress) {}
+    public void onReceivedTitle(@NonNull WebView view, @NonNull String title) {}
+    public void onUriChanged(@NonNull WebView view, @NonNull String uri) {}
+
+    public interface CustomViewCallback {
+        void onCustomViewHidden();
+    }
+
+    // TODO: wire WebKit enter-fullscreen / leave-fullscreen signals to invoke these callbacks.
+    public void onShowCustomView(@NonNull View view, @NonNull CustomViewCallback callback) {}
+    public void onHideCustomView() {}
+}
